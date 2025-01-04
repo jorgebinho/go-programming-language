ae3c249e6d7a6b1aa2c256d759a8391e53c14279
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2025-01-04 11:55:38 -0300

    🧑‍💻 Replace deprecated function

--- chapter1/fetchAll/fetchAll.go
+++ chapter1/fetchAll/fetchAll.go
@@ -5,7 +5,6 @@ package main
 import (
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"os"
 	"time"
@@ -31,7 +30,7 @@ func fetch(url string, ch chan<- string) {
 		return
 	}
 
-	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	nbytes, err := io.Copy(io.Discard, resp.Body)
 	resp.Body.Close()
 	if err != nil {
 		ch <- fmt.Sprintf("while reading %s: %v", url, err)
